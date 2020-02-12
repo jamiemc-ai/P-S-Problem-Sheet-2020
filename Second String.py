df825439a417aa959f4683a1203a295021d4068a
jamiemc-ai <60468556+jamiemc-ai@users.noreply.github.com>
2020-02-12 11:57:16 +0000

    Add files via upload

---
+++ Second String.py
@@ -0,0 +1,7 @@
+# Write a program that asks a user to input 
+# a string and outputs every second letter 
+# in reverse order
+
+sentence = input("enter sentence:")
+sentence = [::-1]
+print("here is youre string reversed and every second letter is :")
